76536339d51f6b36fec77da14b7015982bf77a4f
Tareq Khandakar <tariqul.khandaker@northsouth.edu>
2019-05-04 00:33:47 +0600

    counter

---
+++ VisitorCounter_AutomaticLight_On_Off.ino
@@ -0,0 +1,70 @@
+
+
+#include<LiquidCrystal.h>
+LiquidCrystal lcd(12,11,5,4,3,2);
+
+#define in 14
+#define out 19
+#define relay 1
+
+int count=0;
+
+void IN()
+{
+    count++;
+    lcd.clear();
+    lcd.print("Person In Room:");
+    lcd.setCursor(0,1);
+    lcd.print(count);
+    delay(1000);
+}
+
+void OUT()
+{
+  count--;
+    lcd.clear();
+    lcd.print("Person In Room:");
+    lcd.setCursor(0,1);
+    lcd.print(count);
+    delay(1000);
+}
+
+void setup()
+{
+  analogWrite(6,60);
+  digitalWrite(9,80);
+  lcd.begin(16,2);
+  lcd.print("Visitor Counter");
+  delay(2000);
+  pinMode(in, INPUT);
+  pinMode(out, INPUT);
+  pinMode(relay, OUTPUT);
+  lcd.clear();
+  lcd.print("Person In Room:");
+  lcd.setCursor(0,1);
+  lcd.print(count);
+}
+
+void loop()
+{  
+  
+  if(digitalRead(in)==LOW)
+  IN();
+  if(digitalRead(out)==LOW)
+  OUT();
+  
+  if(count<=0)
+  {
+    lcd.clear();
+    digitalWrite(relay, LOW);
+    lcd.clear();
+    lcd.print("Nobody In Room");
+    lcd.setCursor(0,1);
+    lcd.print("Light Is Off");
+    delay(200);
+  }
+  
+  else
+    digitalWrite(relay, HIGH);
+  
+}
